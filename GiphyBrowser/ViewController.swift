e7b827596911b86a5b73f12c4e12a05340b4a172
Artem Shuba <a.shuba@outlook.com>
2019-05-30 08:43:25 +0200

    Initial Commit

---
+++ GiphyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiphyBrowser
+//
+//  Created by Artem Shuba on 30/05/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
